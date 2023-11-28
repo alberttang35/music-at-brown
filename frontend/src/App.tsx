ecdbb4f2a073a9f0f677ef0d96c8e0af46fb769c
albert <adt2004c@gmail.com>
2023-11-27 23:02:54 -0500

    starter code

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from "react";
+import "./styles/App.css";
+// import REPL from "./components/REPL";
+
+// REMEMBER TO PUT YOUR API KEY IN A FOLDER THAT IS GITIGNORED!!
+// (for instance, /src/private/api_key.tsx)
+// import {API_KEY} from "./private/api_key"
+
+function App() {
+  return <div className="App">bruh</div>;
+}
+
+export default App;
