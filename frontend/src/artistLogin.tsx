89dc752b79fc136500de56f6e4fd420d62e242df
nick kim <nicolas_kim@brown.edu>
2023-11-29 23:28:43 -0500

    feat: added in image display

--- frontend/src/artistLogin.tsx
+++
@@ -1,8 +0,0 @@
-// features:
-// - other nested components:
-// 1. input box for event submission. should have three or so fields you can type into/select (png upload for image, venue location, venue date)
-// 2. artist profile image, editable
-// 3. artist bio, editable
-export default function artistLogin() {
-  return <div></div>;
-}
--- frontend/src/searchBar.tsx
+++
@@ -1,5 +0,0 @@
-
-// searchBar function. inspire from replInput function from other sprints
-export default function SearchBar() {
-    return (<div></div>)
-}
