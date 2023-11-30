e7594b4992388eb3a7af66eeb865487d34137b46
hmin26 <henry_min@brown.edu>
2023-11-29 22:02:34 -0500

    feat: started events

--- frontend/src/types.tsx
+++ frontend/src/types.tsx
@@ -1,19 +1,27 @@
-// Type denoting the types of output as a message or data
-export type ArtistContent = {
-    message?: string;
-    data?: string [][];
-}
+// // Type denoting the types of output as a message or data
+// export type ArtistContent = {
+//     message?: string;
+//     data?: string [][];
+// }
   
-  // Type denoying the inputs for the history as a command or output content
-export type ArtistEntry = {
-    command: string;
-    output: ArtistContent;
-};
+//   // Type denoying the inputs for the history as a command or output content
+// export type ArtistEntry = {
+//     command: string;
+//     output: ArtistContent;
+// };
 
 export type Artist = {
     name: string;
     links: string;
     image: string;
     bio: string;
+}
+
+export type Event = {
+    name: string;
+    data: string;
+    image: string;
+    location: string;
+    link: string;
 }
   
